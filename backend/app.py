4c928e35830f8b7b746f06f093b5fa664f15af99
Neeraj <uhodigas@fryferno.com>
2024-05-26 19:20:19 -0400

    cors comment out

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from pytube import YouTube
 import os
 
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+#CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
 
 # Serve the React app
 @app.route("/", defaults={"path": ""})
